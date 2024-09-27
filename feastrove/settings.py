dec2117793f05cfa09f3ffa2b64fdc25361e13f4
Vaisakh P V <vaisakhpv033@gmail.com>
2024-09-27 13:45:49 +0530

    static files setup, home.html and base.html added, user and Userprofile model created, media files setup

--- feastrove/settings.py
+++ feastrove/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'feastrove.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -85,6 +85,8 @@ DATABASES = {
         'HOST': config('DB_HOST')
     }
 }
+
+AUTH_USER_MODEL = 'accounts.User'
 
 
 # Password validation
@@ -122,6 +124,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR/'static'
+STATICFILES_DIRS = [
+    'feastrove/static'
+]
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR/'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
